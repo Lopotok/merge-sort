ee9888ddeeb55318d526deeee169c0f32ff7b4ed
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-05 20:15:45 +0200

    method reardy for test

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,60 @@ public class Main {
         System.out.printf("Merge Sort -Enyimé");
 
 
+        int[] array = {1, 4, 3, 563, -2, 5, -12, 1, 23, 45, 45, 6, 7, 34, -5, 2412, 12};
+        array = mergeSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
     public static boolean isSorted(int[] array) {
-        for (int i = 0; i < array.length-1; i++) {
-            if (array[i]>array[i+1]){
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
-        } return true;
+        }
+        return true;
     }
 
     public static int[] merge(int[] left, int[] right) {
-    return null;
+        int[] array = new int[left.length + right.length];
+        int arrayIndex = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        while (arrayIndex < array.length &&
+                rightIndex < right.length &&
+                leftIndex < left.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                array[arrayIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                array[arrayIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            arrayIndex++;
+        }
+        while (rightIndex < right.length) {
+            array[arrayIndex] = right[rightIndex];
+            rightIndex++;
+            arrayIndex++;
+        }
+        while (leftIndex < left.length) {
+            array[arrayIndex] = left[leftIndex];
+            leftIndex++;
+            arrayIndex++;
+        }
+        return array;
     }
-    public static int [] mergeSort ( int [] array){
-        if ( isSorted(array)){
+
+    public static int[] mergeSort(int[] array) {
+        if (isSorted(array)) {
             return array;
         }
-        int [] left= Arrays.copyOfRange(array,0,array.length/2);
-        int [] right= Arrays.copyOfRange(array,array.length/2,array.length);
+        int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
         left = mergeSort(left);
         right = mergeSort(right);
-        int []merge = merge(left,right);
-        return null;
+        int[] merged = merge(left, right);
+        return merged;
     }
 }
+
